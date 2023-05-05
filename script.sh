2e74e80b7ff3b4c14b4e61637b7a1d586b776af5
Nolovenodie丶 <nolovenodie@outlook.com>
2023-05-05 16:02:52 +0800

    Update 服务端Docker脚本改为增量更新

--- script.sh
+++ script.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 # 创建emby-crx目录并下载所需文件
-mkdir emby-crx
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/css/style.css -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/common-utils.js -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/jquery-3.6.0.min.js -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/md5.min.js -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/content/main.js -P emby-crx/
+mkdir -p emby-crx
+wget -N https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/css/style.css -P emby-crx/
+wget -N https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/common-utils.js -P emby-crx/
+wget -N https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/jquery-3.6.0.min.js -P emby-crx/
+wget -N https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/md5.min.js -P emby-crx/
+wget -N https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/content/main.js -P emby-crx/
 
 # 读取index.html文件内容
 content=$(cat index.html)
 
-# 定义要插入的代码
-code='<link rel="stylesheet" id="theme-css"  href="emby-crx/style.css" type="text/css" media="all" />\n<script src="emby-crx/common-utils.js"></script>\n<script src="emby-crx/jquery-3.6.0.min.js"></script>\n<script src="emby-crx/md5.min.js"></script>\n<script src="emby-crx/main.js"></script>'
+# 检查index.html是否包含emby-crx
+if [[ "$content" == *"emby-crx"* ]]; then
+    echo "Index.html already contains emby-crx, skipping insertion."
+else
+    # 定义要插入的代码
+    code='<link rel="stylesheet" id="theme-css" href="emby-crx/style.css" type="text/css" media="all" />\n<script src="emby-crx/common-utils.js"></script>\n<script src="emby-crx/jquery-3.6.0.min.js"></script>\n<script src="emby-crx/md5.min.js"></script>\n<script src="emby-crx/main.js"></script>'
 
-# 在</head>之前插入代码
-new_content=$(echo -e "${content/<\/head>/$code<\/head>}")
+    # 在</head>之前插入代码
+    new_content=$(echo -e "${content/<\/head>/$code<\/head>}")
 
-# 将新内容写入index.html文件
-echo -e "$new_content" > index.html
+    # 将新内容写入index.html文件
+    echo -e "$new_content" > index.html
+fi
